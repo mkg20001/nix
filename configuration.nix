861877cd999848498bf3646953d5ed18dd7f8bb3
Maciej Krüger <mkg20001@gmail.com>
2019-11-19 11:13:38 +0100

    feat: mkg overlay

--- configuration.nix
+++ configuration.nix
@@ -6,98 +6,103 @@
 
 with lib;
 
-{
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+let
+  url = "https://github.com/mkg20001/mkgpkgs/archive/master.tar.gz";
+  mkgOverlay = (import (builtins.fetchTarball url));
+in
+  {
+    imports =
+      [ # Include the results of the hardware scan.
+        ./hardware-configuration.nix
+      ];
+
+    nixpkgs.overlays = [ mkgOverlay ];
+
+    # Use the systemd-boot EFI boot loader.
+    # boot.loader.systemd-boot.enable = true;
+    # boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.grub.enable = true;
+    boot.loader.grub.device = "/dev/sdc";
+
+    # networking.hostName = "nixos"; # Define your hostname.
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+    # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+    # Per-interface useDHCP will be mandatory in the future, so this generated config
+    # replicates the default behaviour.
+    networking.useDHCP = false;
+
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+    # Select internationalisation properties.
+    i18n = {
+      consoleFont = "Lat2-Terminus16";
+      consoleKeyMap = "de";
+      defaultLocale = "de_DE.UTF-8";
+    };
+
+    # Set your time zone.
+    time.timeZone = "Europe/Berlin";
+
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+      wget vim nano htop nload iotop git tree
     ];
 
-  # Use the systemd-boot EFI boot loader.
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sdc";
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # programs.mtr.enable = true;
+    # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
-  # networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    # List services that you want to enable:
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
+    # Enable the OpenSSH daemon.
+    services.openssh.enable = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # networking.firewall.enable = false;
 
-  # Select internationalisation properties.
-  i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "de";
-    defaultLocale = "de_DE.UTF-8";
-  };
+    # Enable CUPS to print documents.
+    services.printing.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "Europe/Berlin";
+    # Enable sound.
+    sound.enable = true;
+    hardware.pulseaudio.enable = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    wget vim nano htop nload iotop git tree
-  ];
+    # Enable the X11 windowing system.
+    services.xserver.enable = true;
+    services.xserver.layout = "de";
+    # services.xserver.xkbOptions = "eurosign:e";
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+    # Enable touchpad support.
+    services.xserver.libinput.enable = true;
 
-  # List services that you want to enable:
+    # Enable the KDE Desktop Environment.
+    # services.xserver.displayManager.sddm.enable = true;
+    # services.xserver.desktopManager.plasma5.enable = true;
+    # services.xserver.displayManager.gdm3.enable = true;
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.maciej = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+    # TODO: clean
+    networking.networkmanager.enable = true;
+    networking.wireless.enable = mkForce false;
+    powerManagement.enable = true;
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+    # This value determines the NixOS release with which your system is to be
+    # compatible, in order to avoid breaking some software such as database
+    # servers. You should change this only after NixOS release notes say you
+    # should.
+    system.stateVersion = "19.09"; # Did you read the comment?
 
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.layout = "de";
-  # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable touchpad support.
-  services.xserver.libinput.enable = true;
-
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
-  # services.xserver.displayManager.gdm3.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.maciej = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  };
-
-  # TODO: clean
-  networking.networkmanager.enable = true;
-  networking.wireless.enable = mkForce false;
-  powerManagement.enable = true;
-
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "19.09"; # Did you read the comment?
-
-}
-
+  }
