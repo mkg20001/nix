abaf20c12290e74581d0b37c623a2b872f12d91b
Maciej Krüger <mkg20001@gmail.com>
2019-11-26 03:10:26 +0100

    feat: add pc config

--- devices/pc/index.nix
+++ devices/pc/index.nix
@@ -6,5 +6,14 @@
 {
   imports = [];
 
+  # Name the child
   networking.hostName = "mkg-pc";
+
+  # Use the systemd-boot EFI boot loader.
+  # boot.loader.systemd-boot.enable = true;
+  # boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use MBR
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sdc";
 }
