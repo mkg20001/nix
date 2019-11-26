c3ac5b4b66e6a1c687c8e18024c19346d9ab177b
Maciej Krüger <mkg20001@gmail.com>
2019-11-26 05:52:56 +0100

    merge

--- conf/merged.nix
+++ conf/merged.nix
@@ -1,4 +1,4 @@
 {
-  imports = [ ./1-basic/console-setup.nix ./1-basic/cron.nix ./1-basic/localization.nix ./1-basic/nix-version.nix ./1-basic/user.nix ./2-net/basic.nix ./2-net/firewall.nix ./2-net/ssh.nix ./3-tools/diagnostics.nix ./3-tools/overmind.nix ./3-tools/sysadmin-tools.nix ./3-tools/tmux.nix ./4-apps/atom.nix ./4-apps/browsers.nix ./4-apps/coding-tools.nix ./4-apps/im.nix ./4-apps/utils.nix ./4-apps/voice.nix ./4-apps/work.nix ./6-desktop/basic.nix ./6-desktop/cinnamon.nix ./6-desktop/gdm.nix  ];
+  imports = [ ./1-basic/console-setup.nix ./1-basic/cron.nix ./1-basic/localization.nix ./1-basic/nix-version.nix ./1-basic/user.nix ./2-net/basic.nix ./2-net/firewall.nix ./2-net/ssh.nix ./3-tools/diagnostics.nix ./3-tools/overmind.nix ./3-tools/sysadmin-tools.nix ./3-tools/tmux.nix ./4-apps/atom.nix ./4-apps/browsers.nix ./4-apps/coding-tools.nix ./4-apps/im.nix ./4-apps/utils.nix ./4-apps/voice.nix ./4-apps/work.nix ./6-desktop/basic.nix ./6-desktop/cinnamon.nix ./6-desktop/gdm.nix ./9-misc/printer.nix  ];
 }
 
