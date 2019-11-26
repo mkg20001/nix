0b082e66af3232b9f0b97b678076d59d1b607176
Maciej Krüger <mkg20001@gmail.com>
2019-11-26 07:59:18 +0100

    fix: cjdns

--- conf/2-net/cjdns.nix
+++ conf/2-net/cjdns.nix
@@ -10,5 +10,18 @@ with lib;
   imports = [];
 
   # Enable the cjdns daemon.
-  services.cjdns.enable = true;
+  services.cjdns = {
+    enable = true;
+
+    ETHInterface = {
+      bind = "all";
+      beacon = 2; # 0=off, 1=accept, 2=accept & send
+      # connectTo = {};
+    };
+
+    UDPInterface = {
+      bind = "0.0.0.0:12024";
+      # connectTo = {}; # private config
+    };
+  };
 }
