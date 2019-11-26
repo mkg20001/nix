74c7e390abfaa728252dffa98bf7c114ecce136d
Maciej Krüger <mkg20001@gmail.com>
2019-11-26 06:25:15 +0100

    feat: enable gnome bits

--- conf/6-desktop/gnome-bits.nix
+++ conf/6-desktop/gnome-bits.nix
@@ -1,6 +1,6 @@
 # 6-gnome-bits
 # Add gnome bits and pieces like GOA
-# docref: TODO <nixpkgs/..>
+# docref: <nixpkgs/nixos/modules/services/x11/desktop-managers/gnome3.nix>
 
 { config, lib, pkgs, ... }:
 
@@ -9,5 +9,6 @@ with lib;
 {
   imports = [];
 
-  
+  config.services.gnome3.core-os-services.enable = true;
+  config.services.gnome3.core-utilities.enable = true;
 }
