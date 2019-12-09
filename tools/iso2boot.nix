b158c3d872278cc8b17125bbb3c6a31e73bf8e2b
Maciej Krüger <mkg20001@gmail.com>
2019-12-09 20:14:01 +0100

    fix: wrapper

--- tools/iso2boot.nix
+++ tools/iso2boot.nix
@@ -18,11 +18,14 @@ stdenv.mkDerivation {
     mtools # mmd
   ];
 
-  nativeBuildInputs = [ makeWrapper ];
+  nativeBuildInputs = [
+    makeWrapper
+  ];
 
   installPhase = ''
     sed "s|@@GEFI@@|${grub2_efi}|g" -i iso2boot.sh
     sed "s|@@GRUB@@|${grub2_full}|g" -i iso2boot.sh
-    install -D iso2boot.sh $out/bin/iso2boot
+    install -D iso2boot.sh $out/iso2boot
+    wrapProgram $out/iso2boot $out/bin/iso2boot
     '';
 }
