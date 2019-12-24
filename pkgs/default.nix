85981011145d642a1fc60efbffdbad0f3274503c
Maciej Krüger <mkg20001@gmail.com>
2019-12-24 05:24:50 +0100

    feat: backport pango

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,7 +4,7 @@ let
     # override pangox_compat instead?
     anydesk = pkgs.anydesk.override {
       pangox_compat = pkgs.pangox_compat.override {
-        pango = pkgs.callPackage ./pango-for-anydesk.nix { };
+        pango = pkgs.callPackage ./backportz/pango.nix { };
       };
     };
     iso2boot = pkgs.callPackage ./iso2boot { };
