0d1fcaea04fc14d50b7f6889207da36e4a81655b
Maciej Krüger <mkg20001@gmail.com>
2019-12-25 08:20:19 +0100

    fix: ssh perms

--- .ci/main.sh
+++ .ci/main.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 mkdir /root/.ssh
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
 echo "$ID_RSA" > /root/.ssh/id_rsa
+chmod 400 /root/.ssh/id_rsa
 sed "s|sandbox = false|sandbox = true|g" -i /etc/nix/nix.conf
 
 sh .ci/setup-cache.sh
