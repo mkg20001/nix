77b459e1223d856833b430db39a53b8938c5af16
Maciej Krüger <mkg20001@gmail.com>
2019-12-25 05:46:10 +0100

    fix: use sandbox

--- .ci/main.sh
+++ .ci/main.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 mkdir /root/.ssh
 echo "$ID_RSA" > /root/.ssh/id_rsa
+sed "s|sandbox = false|sandbox = true|g" -i /etc/nix/nix.conf
 
 sh .ci/setup-cache.sh
 sh .ci/setup-repo.sh
