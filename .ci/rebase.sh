89c7733989e6f333bf47eb182fe56c25fc7c7781
Maciej Krüger <mkg20001@gmail.com>
2019-12-25 07:26:40 +0100

    fix: remove unnecesarry checkout

--- .ci/rebase.sh
+++ .ci/rebase.sh
@@ -6,7 +6,6 @@ set -euo pipefail
 git -C ../nixpkgs remote add upstream https://github.com/nixos/nixpkgs || true
 
 git -C ../nixpkgs fetch upstream
-git -C ../nixpkgs checkout origin/mkg-patch-a
 git -C ../nixpkgs reset --hard origin/mkg-patch
 git -C ../nixpkgs rebase upstream/master
 git -C ../nixpkgs branch -D mkg-patch-a || true
