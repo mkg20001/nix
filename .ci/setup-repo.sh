8f21c77507fe1abf879cf079723715d94e74b8f4
Maciej Krüger <mkg20001@gmail.com>
2019-12-25 05:55:56 +0100

    fix: commits

--- .ci/setup-repo.sh
+++ .ci/setup-repo.sh
@@ -10,5 +10,9 @@ fi
 
 git -C ../nixpkgs rebase --abort || true
 git -C ../nixpkgs fetch origin
+git -C ../nixpkgs checkout origin/mkg-patch
 git -C ../nixpkgs reset --hard origin/mkg-patch
 git -C ../nixpkgs clean -dxf
+
+git config --global user.email "mkg20001@gmail.com"
+git config --global user.name "Maciej Krüger"
