2a501e12e6e354ab570716733b1ba9cebba018bc
Maciej Krüger <mkg20001@gmail.com>
2019-12-25 07:16:55 +0100

    fix: rebase push

--- .ci/rebase.sh
+++ .ci/rebase.sh
@@ -9,6 +9,8 @@ git -C ../nixpkgs fetch upstream
 git -C ../nixpkgs checkout origin/mkg-patch-a
 git -C ../nixpkgs reset --hard origin/mkg-patch
 git -C ../nixpkgs rebase upstream/master
+git -C ../nixpkgs branch -D mkg-patch-a
+git -C ../nixpkgs checkout -b mkg-patch-a
 
 bash .ci/rebuild.sh
 
