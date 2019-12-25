acf08969f4a558564d62b35e7408833312b8cdca
Maciej Krüger <mkg20001@gmail.com>
2019-12-25 05:25:52 +0100

    fix: ci nixpkgs path

--- .ci/rebuild.sh
+++ .ci/rebuild.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 nix-build -A all default.nix -I nixpkgs=$PWD/../nixpkgs -v -j auto | cachix push mkg20001
 pushd pkgs
-nix-build default.nix -I nixpkgs=$PWD/../nixpkgs -v -j auto | cachix push mkg20001
+nix-build default.nix -I nixpkgs=$PWD/../../nixpkgs -v -j auto | cachix push mkg20001
 popd
