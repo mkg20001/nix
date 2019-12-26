9668614dffd1dfe5231726e60cb67eaf0bd6a599
Maciej Krüger <mkg20001@gmail.com>
2019-12-26 05:47:16 +0100

    feat: extra substituers

--- .ci/rebuild.sh
+++ .ci/rebuild.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-nix -v -j auto build -f default.nix all -I nixpkgs=$PWD/../nixpkgs | cachix push mkg20001
+nix -v -j auto build -f default.nix all -I nixpkgs=$PWD/../nixpkgs --extra-substituters file:///cache/store | cachix push mkg20001
 pushd pkgs
-nix -v -j auto build -f default.nix -I nixpkgs=$PWD/../nixpkgs | cachix push mkg20001
+nix -v -j auto build -f default.nix -I nixpkgs=$PWD/../nixpkgs --extra-substituters file:///cache/store | cachix push mkg20001
 popd
