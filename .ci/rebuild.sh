c9c54fad756bca3aec4b5a0f8122c825b9210ffd
Maciej Krüger <mkg20001@gmail.com>
2019-12-26 07:15:36 +0100

    fix: upload results

--- .ci/rebuild.sh
+++ .ci/rebuild.sh
@@ -2,7 +2,9 @@
 
 set -euo pipefail
 
-nix -v -j auto build -f default.nix all -I nixpkgs=$PWD/../nixpkgs --extra-substituters file:///cache/store?trusted=1 --trusted-substituters file:///cache/store?trusted=1 | cachix push mkg20001
-pushd pkgs
-nix -v -j auto build -f default.nix -I nixpkgs=$PWD/../../nixpkgs --extra-substituters file:///cache/store?trusted=1 --trusted-substituters file:///cache/store?trusted=1 | cachix push mkg20001
+nix -v -j auto build -f default.nix all -I nixpkgs=$PWD/../nixpkgs --extra-substituters file:///cache/store?trusted=1 --trusted-substituters file:///cache/store?trusted=1
+nix-build -A all -I nixpkgs=$PWD/../nixpkgs | cachix push mkg20001
+pushd pkgs1
+nix -v -j auto build -f default.nix -I nixpkgs=$PWD/../../nixpkgs --extra-substituters file:///cache/store?trusted=1 --trusted-substituters file:///cache/store?trusted=1
+nix-build -I nixpkgs=$PWD/../../nixpkgs | cachix push mkg20001
 popd
