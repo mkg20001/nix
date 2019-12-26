f0b5cc36b5bce5561a2bdf6b0df336af6766cffe
Maciej Krüger <mkg20001@gmail.com>
2019-12-26 13:23:37 +0100

    fix: iso

---
+++ devices/iso/README.txt
@@ -0,0 +1,14 @@
+=== INSTALLATION ===
+
+User password: install
+
+Commands:
+
+- nixos-generate-config --root /mnt
+  Generate nixos config on /mnt
+
+- DEVICE=device-name bootstrap
+  Bootstrap config for device-name on $DEVICE
+
+- nixos-install --root /mnt
+  Install or rebuild os on /mnt
--- devices/iso/default.nix
+++ devices/iso/default.nix
@@ -1,7 +1,53 @@
-{ ... }:
+{ config, lib, pkgs, ... }:
 
+let
+  README = (pkgs.writeText "README.txt" (builtins.readFile ./README.txt));
+in
 {
   imports = [
     <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-base.nix>
+  ];
+
+  # Name the child
+  networking.hostName = "mkg-iso";
+
+  # Auto-login & default pw
+  services.xserver.displayManager.gdm.autoLogin = {
+    enable = true;
+    user = "maciej";
+  };
+  users.users.maciej.initialPassword = "install";
+
+  # Whitelist wheel users to do anything
+  # This is useful for things like pkexec
+  #
+  # WARNING: this is dangerous for systems
+  # outside the installation-cd and shouldn't
+  # be used anywhere else.
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (subject.isInGroup("wheel")) {
+        return polkit.Result.YES;
+      }
+    });
+  '';
+
+  # Desktop
+  system.activationScripts.installerDesktop = let
+    homeDir = "/home/maciej/";
+    desktopDir = homeDir + "Desktop/";
+
+  in ''
+    mkdir -p ${desktopDir}
+    chown nixos ${homeDir} ${desktopDir}
+
+    ln -sfT ${pkgs.gparted}/share/applications/gparted.desktop ${desktopDir + "gparted.desktop"}
+    ln -sfT ${pkgs.gnome3.gnome_terminal}/share/applications/gnome-terminal.desktop ${desktopDir + "gnome-terminal.desktop"}
+    ln -sfT ${README} ${desktopDir + "README.txt"}
+  '';
+
+  environment.systemPackages = [
+    (pkgs.writeShellScriptBin "bootstrap" (builtins.readFile ../../bootstrap.sh))
+    README
   ];
 }
