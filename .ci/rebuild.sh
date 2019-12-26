d96dcac47d2b71434a895972fd39cbf5ff333261
Maciej Krüger <mkg20001@gmail.com>
2019-12-26 07:10:37 +0100

    fix: substituters

--- .ci/rebuild.sh
+++ .ci/rebuild.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-nix -v -j auto build -f default.nix all -I nixpkgs=$PWD/../nixpkgs --extra-substituters file:///cache/store?trusted=1 | cachix push mkg20001
+nix -v -j auto build -f default.nix all -I nixpkgs=$PWD/../nixpkgs --extra-substituters file:///cache/store?trusted=1 --trusted-substituters file:///cache/store?trusted=1 | cachix push mkg20001
 pushd pkgs
-nix -v -j auto build -f default.nix -I nixpkgs=$PWD/../../nixpkgs --extra-substituters file:///cache/store?trusted=1 | cachix push mkg20001
+nix -v -j auto build -f default.nix -I nixpkgs=$PWD/../../nixpkgs --extra-substituters file:///cache/store?trusted=1 --trusted-substituters file:///cache/store?trusted=1 | cachix push mkg20001
 popd
