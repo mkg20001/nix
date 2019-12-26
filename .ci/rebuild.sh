579a81baf27c033e15e4dbe3509ccc91dd5cb142
Maciej Krüger <mkg20001@gmail.com>
2019-12-26 05:41:10 +0100

    fix: rebuild

--- .ci/rebuild.sh
+++ .ci/rebuild.sh
@@ -3,7 +3,6 @@
 set -euo pipefail
 
 nix -v -j auto build -f default.nix all -I nixpkgs=$PWD/../nixpkgs | cachix push mkg20001
-nix build -A all default.nix -I nixpkgs=$PWD/../nixpkgs -v -j auto | cachix push mkg20001
 pushd pkgs
 nix -v -j auto build -f default.nix -I nixpkgs=$PWD/../nixpkgs | cachix push mkg20001
 popd
