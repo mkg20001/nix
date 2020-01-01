db72e086b01cb93c366865e36ff68f345c1a2824
Maciej Krüger <mkg20001@gmail.com>
2020-01-01 09:37:19 +0100

    fix: fast

--- fast.sh
+++ fast.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-nixos-rebuild switch -I nixpkgs=/etc/nixpkgs -v --fast 2>&1 | grep -v evaluating
+nixos-rebuild switch -I nixpkgs=/etc/nixpkgs -v --fast --show-trace 2>&1 | grep -v evaluating
