897ddfad332e9f252ca0db6f78196de264d3df77
Maciej Krüger <mkg20001@gmail.com>
2020-01-21 02:07:50 +0100

    feat: printing

--- conf/sub/printer.nix
+++ conf/sub/printer.nix
@@ -1,11 +1,40 @@
 { config, lib, pkgs, ... }:
 
 {
+  services.system-config-printer.enable = true;
+  environment.systemPackages = with pkgs; [ system-config-printer ];
+
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [ gutenprint hplip samsungUnifiedLinuxDriver splix brlaser brgenml1lpr brgenml1cupswrapper cups-googlecloudprint ];
+    # this enables avahi+browsed autodiscover
+    browsing = true;
+    defaultShared = true;
+    extraConf = ''
+BrowseDNSSDSubTypes _cups,_print
+BrowseLocalProtocols all
+BrowseRemoteProtocols all
+CreateIPPPrinterQueues All
+    '';
+    browsedConf = ''
+BrowseDNSSDSubTypes _cups,_print
+BrowseLocalProtocols all
+BrowseRemoteProtocols all
+CreateIPPPrinterQueues All
+
+BrowseProtocols all
+    '';
+  };
 
   # And sane to scan
   hardware.sane.enable = true;
+
+  # Cups network printing
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+  };
 
   hardware.sane.brscan4 = {
     enable = true;
