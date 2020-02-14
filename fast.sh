1e541bfb7f5d1f72dc3b58aff9d7815ec4a34aa7
Maciej Krüger <mkg20001@gmail.com>
2020-02-14 14:19:09 +0100

    feat: fast changes, theme

--- fast.sh
+++ fast.sh
@@ -2,4 +2,12 @@
 
 set -e
 
-nixos-rebuild switch -I nixpkgs=/etc/nixpkgs -v --fast --show-trace 2>&1 | grep -v evaluating
+if [ ! -z "$PR_NIXPKGS" ]; then
+  NIXPKGS="/home/maciej/Projekte/nixpkgs"
+fi
+
+if [ -z "$NIXPKGS" ]; then
+  NIXPKGS="/etc/nixpkgs"
+fi
+
+nixos-rebuild switch -I "nixpkgs=$NIXPKGS" -v --fast --show-trace "$@" 2>&1 | grep -v evaluating
