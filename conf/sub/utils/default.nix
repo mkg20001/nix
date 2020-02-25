ef9867cb632beb64d6a1132c10f1dc03e24a8c7f
Maciej Krüger <mkg20001@gmail.com>
2020-02-25 16:16:21 +0100

    feat: make sysadmin-tools common

--- conf/sub/utils/default.nix
+++ conf/sub/utils/default.nix
@@ -2,6 +2,12 @@
 
 with lib;
 
+let
+  common = builtins.fetchGit {
+    url = "https://github.com/mkg20001/mkg-nix";
+    rev = "2947964be1b86ee8e24feaddc94c342a00183f0e";
+  };
+in
 {
   imports = [
     ./android.nix
@@ -9,7 +15,7 @@ with lib;
     ./docker.nix
     ./lxd.nix
     ./qemu.nix
-    ./sysadmin-tools.nix
+    "${common}/sysadmin-tools.nix"
     ./tools.nix
     ./vbox.nix
   ];
