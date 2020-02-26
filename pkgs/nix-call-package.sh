5f6aff98288c75b2ef8678d85b6dcfc359c26af2
Maciej Krüger <mkg20001@gmail.com>
2020-02-26 02:58:50 +0100

    another fix

--- pkgs/nix-call-package.sh
+++ pkgs/nix-call-package.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
-EXPR="(import <nixpkgs> {}).callPackage $PATH {}"
+# Usage: $0 <package>
 
+PKG="$1"
+
+set -euo pipefail
+
+if [ -z "$PKG" ]; then
+  echo "Usage: $0 <package>" >&2
+  exit 2
+fi
+
+EXPR="((import <nixpkgs> {}).callPackage $PKG {})"
+
+shift
 nix-build -j auto -E "$EXPR" "$@"
