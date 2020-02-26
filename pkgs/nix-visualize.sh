7e84923b87dcc907c2eb6f8c341956615f5d8c60
Maciej Krüger <mkg20001@gmail.com>
2020-02-26 03:18:39 +0100

    feat: variable fmt

--- pkgs/nix-visualize.sh
+++ pkgs/nix-visualize.sh
@@ -6,9 +6,11 @@ OUT="$2"
 set -euo pipefail
 
 if [ -z "$OUT" ]; then
-  echo "Usage: $0 <store-path> <out(.png)>" >&2
+  echo "Usage: $0 <store-path> <out>" >&2
   echo "e.g: $0 /run/current-system current-system.png" >&2
   exit 2
 fi
 
-nix-store -q --graph "$STORE_PATH" | dot -Tpng -o "$OUT"
+FMT=$(echo "$OUT" | sed -r "s|.+\\.([a-z0-9]+)|\1|g")
+
+nix-store -q --graph "$STORE_PATH" | dot "-T$FMT" -o "$OUT"
