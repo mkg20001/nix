7a23764a30301307ef1c5d105da91a07f2406369
Maciej Krüger <mkg20001@gmail.com>
2020-03-01 16:53:33 +0100

    fix: wakafix

--- pkgs/atom/default.nix
+++ pkgs/atom/default.nix
@@ -12,9 +12,13 @@ let
   snip = ''
 ${snipStart}
 
-WA_BIN="$ATOM_HOME/packages/wakatime/lib/wakatime-cli/wakatime-cli"
+WA_DIR="$ATOM_HOME/packages/wakatime/lib"
+WA_LIB="$WA_DIR/wakatime.coffee"
+WA_BIN="$WA_DIR/wakatime-cli/wakatime-cli"
+
 if [ -x "$WA_BIN" ]; then
   "${wakafix}/bin/wakafix" "$WA_BIN"
+  sed "s|currentVersion = stderr.trim()|currentVersion = stdout.trim()|g" -i "$WA_LIB"
 fi
   '';
 in
