3fd1fba4b81f3c50f2ef0026e014ceb6af16aa24
Maciej Krüger <mkg20001@gmail.com>
2020-03-18 09:12:03 +0100

    feat: use MY nix

--- conf/default.nix
+++ conf/default.nix
@@ -49,6 +49,7 @@ with (import ./util.nix lib);
   nix = {
     useSandbox = true;
     autoOptimiseStore = true;
+    package = pkgs.nixMkg;
 
     extraOptions = ''
       # In general, outputs must be registered as roots separately. However, even if the output of a derivation is registered as a root, the collector will still delete store paths that are used only at build time (e.g., the C compiler, or source tarballs downloaded from the network). To prevent it from doing so, set this option to true.
