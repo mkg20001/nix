11e86ffb96254d0f9f52008636d61e8ad8335bf7
Maciej Krüger <mkg20001@gmail.com>
2020-03-29 17:39:12 +0200

    feat: update node

--- pkgs/node/pkgs/default.nix
+++ pkgs/node/pkgs/default.nix
@@ -1,10 +1,10 @@
 [
-  { name = "aegir"; version = "21.4.3"; root = ./aegir; }
+  { name = "aegir"; version = "21.4.5"; root = ./aegir; }
   { name = "nodemon"; version = "2.0.2"; root = ./nodemon; }
   { name = "parcel"; version = "1.12.4"; root = ./parcel; }
   { name = "http-server"; version = "0.12.1"; root = ./http-server; }
   { name = "pino-pretty"; version = "3.6.1"; root = ./pino-pretty; }
-  { name = "npm-check-updates"; version = "4.0.4"; root = ./npm-check-updates; }
+  { name = "npm-check-updates"; version = "4.1.0"; root = ./npm-check-updates; }
   { name = "google-font-installer"; version = "1.2.0"; root = ./google-font-installer; }
   { name = "wscat"; version = "4.0.0"; root = ./wscat; }
   { name = "json5"; version = "2.1.2"; root = ./json5; }
@@ -17,8 +17,8 @@
   { name = "stylelint"; version = "13.2.1"; root = ./stylelint; }
   { name = "eslint-config-aegir-standalone"; version = "21.3.0"; root = ./eslint-config-aegir-standalone; }
   { name = "oclif"; version = "1.15.2"; root = ./oclif; }
-  { name = "thingycreate"; version = "0.3.7"; root = ./thingycreate; }
-  { name = "thingycontrol"; version = "0.1.6"; root = ./thingycontrol; }
-  { name = "thingymodulecreate"; version = "0.1.6"; root = ./thingymodulecreate; }
-  { name = "thingysync"; version = "0.1.1"; root = ./thingysync; }
+  { name = "thingycreate"; version = "0.3.9"; root = ./thingycreate; }
+  { name = "thingycontrol"; version = "0.1.8"; root = ./thingycontrol; }
+  { name = "thingymodulecreate"; version = "0.1.8"; root = ./thingymodulecreate; }
+  { name = "thingysync"; version = "0.1.2"; root = ./thingysync; }
 ]
