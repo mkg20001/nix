0fffe041f1fe0f850889363d3beb9670e1fcd0d3
Maciej Krüger <mkg20001@gmail.com>
2020-04-20 06:46:03 +0200

    misc: clean

--- pkgs/firmware-linux-nonfree/default.nix
+++ pkgs/firmware-linux-nonfree/default.nix
@@ -6,7 +6,7 @@ recreatePackage firmwareLinuxNonfree {
   outputHashMode = null;
   outputHashAlgo = null;
   outputHash = null;
-  
+
   postInstall = ''
     rm -rf $out/lib/firmware/netronome         # non-enduser nic vendor
     rm -rf $out/lib/firmware/liquidio          # non-enduser nic vendor
