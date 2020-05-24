074f7d9446688383fa4d75019cddc2182268069d
Maciej Krüger <mkg20001@gmail.com>
2020-05-24 14:28:19 +0200

    feat: add solar

---
+++ cachix/solar.nix
@@ -0,0 +1,12 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://solar.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "solar.cachix.org-1:7j5FItAzYXltOuKmDtHK6gw96LTPUxFs5OlTiRfklJg="
+    ];
+  };
+}
+    
