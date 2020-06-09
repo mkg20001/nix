6bcb47a4c38c744eef6b1e3bdc040ad86d224d46
Maciej Krüger <mkg20001@gmail.com>
2020-06-09 23:18:11 +0200

    feat: use cachix from nixpkgs

--- .ci/setup-cache.sh
+++ .ci/setup-cache.sh
@@ -5,7 +5,8 @@ set -euo pipefail
 mkdir -p /cache/store
 echo "trusted-substituters = " >> /etc/nix/nix.conf
 
-nix-env -iA cachix -f https://cachix.org/api/v1/install
+# nix-env -iA cachix -f https://cachix.org/api/v1/install
+nix-env -iA nixpkgs.cachix
 cachix use mkg20001
 
 sed "s|substituters = |substituters = local?root=/cache/store file:///cache/store |g" -i /etc/nix/nix.conf
