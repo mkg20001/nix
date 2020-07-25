f0eee03d2323a4880c8e9edfecb8260dd907aa8f
Maciej Krüger <mkg20001@gmail.com>
2020-07-26 01:39:59 +0200

    feat: add nix-dev

--- pkgs/node/pkgs.sh
+++ pkgs/node/pkgs.sh
@@ -4,8 +4,10 @@ _() {
   gen_pkg "$1" "$@"
 }
 
-# some packages are commented out due to a bug that doesn't allow for 2500+ deps
+# my very own stuff
+_ nix-dev
 
+# tools
 _ aegir
 _ nodemon
 _ parcel
@@ -20,9 +22,9 @@ _ ndb
 _ 0x
 _ jay-repl
 _ diff-so-fancy
-_ eslint eslint-config-aegir-standalone eslint-plugin-no-only-tests
+_ eslint eslint-config-aegir-standalone
 _ stylelint
-_ eslint-config-aegir-standalone eslint-plugin-no-only-tests
+_ eslint-config-aegir-standalone
 _ oclif
 _ taskbook
 
--- pkgs/node/pkgs/default.nix
+++ pkgs/node/pkgs/default.nix
@@ -1,21 +1,22 @@
 [
-  { name = "aegir"; version = "22.0.0"; root = ./aegir; }
+  { name = "nix-dev"; version = "0.1.0"; root = ./nix-dev; }
+  { name = "aegir"; version = "25.0.0"; root = ./aegir; }
   { name = "nodemon"; version = "2.0.4"; root = ./nodemon; }
   { name = "parcel"; version = "1.12.4"; root = ./parcel; }
   { name = "http-server"; version = "0.12.3"; root = ./http-server; }
-  { name = "pino-pretty"; version = "4.0.0"; root = ./pino-pretty; }
-  { name = "npm-check-updates"; version = "6.0.1"; root = ./npm-check-updates; }
+  { name = "pino-pretty"; version = "4.1.0"; root = ./pino-pretty; }
+  { name = "npm-check-updates"; version = "7.0.2"; root = ./npm-check-updates; }
   { name = "google-font-installer"; version = "1.2.0"; root = ./google-font-installer; }
-  { name = "wscat"; version = "4.0.0"; root = ./wscat; }
+  { name = "wscat"; version = "4.0.1"; root = ./wscat; }
   { name = "json5"; version = "2.1.3"; root = ./json5; }
-  { name = "pkg"; version = "4.4.8"; root = ./pkg; }
+  { name = "pkg"; version = "4.4.9"; root = ./pkg; }
   { name = "ndb"; version = "1.1.5"; root = ./ndb; }
   { name = "0x"; version = "4.9.1"; root = ./0x; }
   { name = "jay-repl"; version = "0.2.2"; root = ./jay-repl; }
-  { name = "diff-so-fancy"; version = "1.2.7"; root = ./diff-so-fancy; }
-  { name = "eslint"; version = "7.1.0"; root = ./eslint; }
-  { name = "stylelint"; version = "13.5.0"; root = ./stylelint; }
-  { name = "eslint-config-aegir-standalone"; version = "21.3.0"; root = ./eslint-config-aegir-standalone; }
+  { name = "diff-so-fancy"; version = "1.3.0"; root = ./diff-so-fancy; }
+  { name = "eslint"; version = "7.5.0"; root = ./eslint; }
+  { name = "stylelint"; version = "13.6.1"; root = ./stylelint; }
+  { name = "eslint-config-aegir-standalone"; version = "25.0.0"; root = ./eslint-config-aegir-standalone; }
   { name = "oclif"; version = "1.16.1"; root = ./oclif; }
   { name = "taskbook"; version = "0.3.0"; root = ./taskbook; }
   { name = "thingycreate"; version = "0.3.9"; root = ./thingycreate; }
