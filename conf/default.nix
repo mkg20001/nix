6bd05871c647da80d28740609c32c8446ece75e8
Maciej Krüger <mkg20001@gmail.com>
2020-08-25 00:22:22 +0200

    feat: add sunshine config correclt

--- conf/default.nix
+++ conf/default.nix
@@ -130,13 +130,34 @@ with (import ./util.nix lib);
   services.sunshine = {
     enable = true;
     extraConfig = ''
-      gc: {}
-      user:
-        gc: {}
-        upgrade: {}
-      system:
-        gc: {}
-        upgrade: {}
+gc:
+  onInterval: true # true=daily, false=never or string containing cron interval
+  onLowSpace: 2147483648 # 2gb, 0 to disable, will trigger every minute and check threshold on /nix/store partition
+  keepDays: 7 # how many days to keep old, unused stuff arround for rollbacks
+  maxFree: 0 # _only_ free _up to_ this much space, 0=ignore
+system:
+  gc: {}
+  upgrade: # affects nixos-rebuild
+    interval: true # true=daily false=never or string containg cron interval
+    silentFetch: true # fetch channels silently instead of asking
+    silentPrepare: true # prefetch already built derivations from cache but don't build anything just yet, will not run on paid networks (e.g. mobile)
+    forceSilentPrepare: false # always run silentPrepare
+    silentApplyForNextBoot: true # build with nixos-rebuild boot so update gets applied on next boot
+    notifyUpdateAvailable: true # make a notification to apply update during current boot
+    rollback:
+      checks:
+        sanityCheck: true # perform checks while the system is running
+        grubFlag: false # add settings to grub to auto-rollback
+      allowAuto: critical # never, critical, always - change when auto-rollbacks are done
+user:
+  gc: {}
+  upgrade: # affects nix-env
+    interval: true # true=daily false=never or string containg cron interval
+    silentFetch: true # fetch channels silently instead of asking
+    silentPrepare: true # prefetch already built derivations from cache but don't build anything just yet, will not run on paid networks (e.g. mobile)
+    silentApply: true # silently build and apply
+    forceSilentPrepare: false # always run silentPrepare
+cooldownMsAfterBoot: 18000000 # 5 min
     '';
   };
 }
