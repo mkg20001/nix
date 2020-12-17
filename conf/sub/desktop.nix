3878b2d29b303bc03e6cddd23ce5fd303e6782fe
Maciej Krüger <mkg20001@gmail.com>
2020-12-17 02:41:42 +0100

    feat: intel vaapi

--- conf/sub/desktop.nix
+++ conf/sub/desktop.nix
@@ -79,8 +79,17 @@ with lib;
   # More stuff
   services.gvfs.enable = true;
 
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
   hardware.opengl = {
     enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
     # driSupport32Bit = true;
   };
 }
