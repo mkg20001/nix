3f1c0bdffd00d9bdd9a868e1c8b11565f43481e2
Maciej Krüger <mkg20001@gmail.com>
2021-01-25 05:58:40 +0100

    chore: upgrade node pkgs

--- pkgs/node/pkgs/default.nix
+++ pkgs/node/pkgs/default.nix
@@ -1,11 +1,11 @@
 [
   { name = "nix-dev"; version = "0.1.2"; root = ./nix-dev; }
-  { name = "aegir"; version = "30.2.0"; root = ./aegir; }
-  { name = "nodemon"; version = "2.0.6"; root = ./nodemon; }
+  { name = "aegir"; version = "30.3.0"; root = ./aegir; }
+  { name = "nodemon"; version = "2.0.7"; root = ./nodemon; }
   { name = "parcel"; version = "1.12.4"; root = ./parcel; }
   { name = "http-server"; version = "0.12.3"; root = ./http-server; }
   { name = "pino-pretty"; version = "4.3.0"; root = ./pino-pretty; }
-  { name = "npm-check-updates"; version = "10.2.5"; root = ./npm-check-updates; }
+  { name = "npm-check-updates"; version = "11.0.2"; root = ./npm-check-updates; }
   { name = "google-font-installer"; version = "1.2.0"; root = ./google-font-installer; }
   { name = "wscat"; version = "4.0.1"; root = ./wscat; }
   { name = "json5"; version = "2.1.3"; root = ./json5; }
@@ -14,12 +14,12 @@
   { name = "0x"; version = "4.10.2"; root = ./0x; }
   { name = "jay-repl"; version = "0.2.2"; root = ./jay-repl; }
   { name = "diff-so-fancy"; version = "1.3.0"; root = ./diff-so-fancy; }
-  { name = "eslint"; version = "7.17.0"; root = ./eslint; }
-  { name = "stylelint"; version = "13.8.0"; root = ./stylelint; }
+  { name = "eslint"; version = "7.18.0"; root = ./eslint; }
+  { name = "stylelint"; version = "13.9.0"; root = ./stylelint; }
   { name = "eslint-config-aegir-standalone"; version = "25.0.0"; root = ./eslint-config-aegir-standalone; }
   { name = "oclif"; version = "1.16.1"; root = ./oclif; }
   { name = "taskbook"; version = "0.3.0"; root = ./taskbook; }
-  { name = "localtunnel"; version = "2.0.0"; root = ./localtunnel; }
+  { name = "localtunnel"; version = "2.0.1"; root = ./localtunnel; }
   { name = "thingycreate"; version = "0.3.9"; root = ./thingycreate; }
   { name = "thingycontrol"; version = "0.1.8"; root = ./thingycontrol; }
   { name = "thingymodulecreate"; version = "0.1.8"; root = ./thingymodulecreate; }
