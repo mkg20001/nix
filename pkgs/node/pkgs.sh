4ecad951de895c1ed17f68c094b7d5dba721228f
Maciej Krüger <mkg20001@gmail.com>
2021-05-14 20:08:04 +0200

    feat: disable some unused stuff

--- pkgs/node/pkgs.sh
+++ pkgs/node/pkgs.sh
@@ -10,17 +10,17 @@ _ nix-dev
 # tools
 # _ aegir
 _ nodemon
-_ parcel
+# _ parcel
 _ http-server
 _ pino-pretty
 _ npm-check-updates
 _ google-font-installer
 _ wscat
-_ json5
+# _ json5
 _ pkg
 _ ndb
 _ 0x
-_ jay-repl
+# _ jay-repl
 _ diff-so-fancy
 _ eslint eslint-config-aegir-standalone
 _ stylelint
@@ -30,7 +30,7 @@ _ taskbook
 _ localtunnel localtunnel
 
 # thingy
-_ thingycreate
-_ thingycontrol
-_ thingymodulecreate
-_ thingysync
+# _ thingycreate
+# _ thingycontrol
+# _ thingymodulecreate
+# _ thingysync
