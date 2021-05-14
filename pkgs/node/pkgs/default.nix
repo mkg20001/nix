b6f1f6fca76a77cb9cdf0b57b04b73f1103e9150
Maciej Krüger <mkg20001@gmail.com>
2021-05-14 20:10:53 +0200

    chore: upgrade deps

--- pkgs/node/pkgs/default.nix
+++ pkgs/node/pkgs/default.nix
@@ -1,17 +1,14 @@
 [
   { name = "nix-dev"; version = "0.1.3"; root = ./nix-dev; }
   { name = "nodemon"; version = "2.0.7"; root = ./nodemon; }
-  # { name = "parcel"; version = ""; root = ./parcel; }
   { name = "http-server"; version = "0.12.3"; root = ./http-server; }
-  { name = "pino-pretty"; version = "4.7.1"; root = ./pino-pretty; }
-  { name = "npm-check-updates"; version = "11.5.10"; root = ./npm-check-updates; }
+  { name = "pino-pretty"; version = "4.8.0"; root = ./pino-pretty; }
+  { name = "npm-check-updates"; version = "11.5.12"; root = ./npm-check-updates; }
   { name = "google-font-installer"; version = "1.2.0"; root = ./google-font-installer; }
   { name = "wscat"; version = "4.0.1"; root = ./wscat; }
-  { name = "json5"; version = "2.2.0"; root = ./json5; }
   { name = "pkg"; version = "5.1.0"; root = ./pkg; }
   { name = "ndb"; version = "1.1.5"; root = ./ndb; }
   { name = "0x"; version = "4.10.2"; root = ./0x; }
-  { name = "jay-repl"; version = "0.2.2"; root = ./jay-repl; }
   { name = "diff-so-fancy"; version = "1.3.0"; root = ./diff-so-fancy; }
   { name = "eslint"; version = "7.26.0"; root = ./eslint; }
   { name = "stylelint"; version = "13.13.1"; root = ./stylelint; }
@@ -19,8 +16,4 @@
   { name = "oclif"; version = "1.17.0"; root = ./oclif; }
   { name = "taskbook"; version = "0.3.0"; root = ./taskbook; }
   { name = "localtunnel"; version = "2.0.1"; root = ./localtunnel; }
-  { name = "thingycreate"; version = "0.3.9"; root = ./thingycreate; }
-  { name = "thingycontrol"; version = "0.1.8"; root = ./thingycontrol; }
-  { name = "thingymodulecreate"; version = "0.1.8"; root = ./thingymodulecreate; }
-  { name = "thingysync"; version = "0.1.2"; root = ./thingysync; }
 ]
