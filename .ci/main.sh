e185e707e523b42d88e5942051da72c026a94f81
Maciej Krüger <mkg20001@gmail.com>
2021-05-18 19:02:45 +0200

    feat: build iso first

--- .ci/main.sh
+++ .ci/main.sh
@@ -17,18 +17,19 @@ sh .ci/tag.sh
 #bash .ci/rebuild.sh
 bash .ci/rebase.sh
 
+if ! which sftp >/dev/null 2>/dev/null; then
+  nix-env -iA nixpkgs.openssh
+fi
+
+nix-build --option build-use-sandbox true -A iso -I nixpkgs=$PWD/../nixpkgs
+cp -v result/iso/* mkg.iso
+(echo rm mkg.iso | sftp -o StrictHostKeyChecking=no -b /dev/stdin sftp://dpl@argon.mkg20001.io:13701/../../var/www/tmp) || true
+echo put mkg.iso | sftp -o StrictHostKeyChecking=no -b /dev/stdin sftp://dpl@argon.mkg20001.io:13701/../../var/www/tmp
+
 nix-build --option build-use-sandbox true -A vme -I nixpkgs=$PWD/../nixpkgs
 cp -v result/*.ova mkg.ova
 (echo rm mkg.ova | sftp -o StrictHostKeyChecking=no -b /dev/stdin sftp://dpl@argon.mkg20001.io:13701/../../var/www/tmp) || true
 echo put mkg.ova | sftp -o StrictHostKeyChecking=no -b /dev/stdin sftp://dpl@argon.mkg20001.io:13701/../../var/www/tmp
 
-nix-build --option build-use-sandbox true -A iso -I nixpkgs=$PWD/../nixpkgs
-cp -v result/iso/* mkg.iso
-
 # nix copy --to file:///cache/store --all -v
 
-if ! which sftp >/dev/null 2>/dev/null; then
-  nix-env -iA nixpkgs.openssh
-fi
-(echo rm mkg.iso | sftp -o StrictHostKeyChecking=no -b /dev/stdin sftp://dpl@argon.mkg20001.io:13701/../../var/www/tmp) || true
-echo put mkg.iso | sftp -o StrictHostKeyChecking=no -b /dev/stdin sftp://dpl@argon.mkg20001.io:13701/../../var/www/tmp
