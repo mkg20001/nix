ab9ffb47e26957ce461adae33fd8e3212b1cb205
Maciej Krüger <mkg20001@gmail.com>
2021-06-06 04:00:31 +0200

    feat: xeredo cache

---
+++ cachix/xeredo.nix
@@ -0,0 +1,11 @@
+{
+  nix = {
+    binaryCaches = [
+      "https://cache.xeredo.it"
+    ];
+    binaryCachePublicKeys = [
+      "cache.xeredo.it-1:Dlh2ON5d64vjGOSc7NTD7/64diyuZczokmGObFRjMvE="
+    ];
+  };
+}
+
